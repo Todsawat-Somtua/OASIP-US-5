1c72b98a559ccfd7129e15c3f68c8e3f97945050
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2022-04-28 04:13:52 +0700

    Database script files

---
+++ oasip-ud-5-database-scripts/oasip-events-table-us-5-week1.sql
@@ -0,0 +1,8 @@
+insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
+value ('Somchai Jaidee(OR-7)', 'somchai.jai@mail.kmutt.ac.th', 2, '2022-5-23 13:30:00');
+insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime, eventNotes)
+value ('SomsriRakdee(SJ-3)', 'somsri.rak@mail.kmutt.ac.th', 'Project Management Clinic', '27 April 2022 9.30', 'ขอปรึกษาปัญหาเพื่อนไม่ช`วยงาน');
+insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
+value ('สมเกียรติ ขยันเรียน กลุ`ม TT-4)', 'somkiat.kay@kmutt.ac.th', 'Database Clinic', '23 May 2022 16.30');
+
+select * from events;
