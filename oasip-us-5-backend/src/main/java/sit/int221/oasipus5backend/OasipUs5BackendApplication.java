e84408e5a469b5c789e83bde3ad763c56743b9a0
NattaponChusriwan <nattapon.chu@mail.kmutt.ac.th>
2022-04-27 16:02:38 +0700

    backend push

---
+++ oasip-us-5-backend/src/main/java/sit/int221/oasipus5backend/OasipUs5BackendApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.oasipus5backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasipUs5BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasipUs5BackendApplication.class, args);
+    }
+
+}
---
+++ oasip-us-5-backend/src/test/java/sit/int221/oasipus5backend/OasipUs5BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int221.oasipus5backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasipUs5BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
