ee47bc25d1f1f7c385f6481fe8b9e477636b8849
NattaponChusriwan <nattapon.chu@mail.kmutt.ac.th>
2022-04-27 17:58:24 +0700

    frontend-firstCommit

---
+++ oasip-us-5-frontend/oasip-frontend/src/views/Home.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onBeforeMount } from "vue";
+
+const eventsList = ref([]);
+
+// Create
+// Read
+const getEvent = async () => {
+  const res = await fetch("/api/events");
+  if (res.status === 200) {
+    eventsList.value = await res.json();
+  } else console.log("error, cannot get data");
+};
+
+onBeforeMount(async () => {
+  await getEvent();
+});
+
+// Update
+// Delete
+</script>
+
+<template>
+  <div>
+      <p v-if="eventsList === []">No schedule</p>
+      <p v-else>มีข้อมูลจ้า</p>
+  </div>
+</template>
+
+<style></style>
