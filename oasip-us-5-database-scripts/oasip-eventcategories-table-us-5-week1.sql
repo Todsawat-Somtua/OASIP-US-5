466ce0d9cf30d9da4c05a8408ec704486ad678ca
NattaponChusriwan <nattapon.chu@mail.kmutt.ac.th>
2022-05-02 16:11:14 +0700

    fixed data

--- oasip-us-5-database-scripts/oasip-eventcategories-table-us-5-week1.sql
+++ oasip-us-5-database-scripts/oasip-eventcategories-table-us-5-week1.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 use oasipdatabase;
 
 insert into eventcategories
@@ -13,18 +13,3 @@ insert into eventcategories (eventCategoryId,eventCategoryName,eventDuration)
 value (5,'Server-side Clinic', 30);
 
 
-
-=======
-insert into eventcategories
-value (1,'Project Management Clinic', 30, 'ตารางนัดหมายนี้ใชKสําหรับนัดหมาย project management clinic ในวิชา INT221 integrated project I ใหKนักศึกษาเตรียมเอกสารที่เกี่ยวขKองเพื่อแสดงระหวaางขอคําปรึกษา');
-insert into eventcategories
-value (2,'DevOps/Infra Clinic', 20, 'Use this event category for DevOps/Infra clinic.');
-insert into eventcategories
-value (3,'Database Clinic', 15, 'ตารางนัดหมายนี้ใชKสําหรับนัดหมาย database clinic ในวิชา INT221 integrated project I');
-insert into eventcategories
-value (4,'Client-side Clinic', 30, 'ตารางนัดหมายนี้ใชKสําหรับนัดหมาย client-side clinic ในวิชา INT221 integrated project I');
-insert into eventcategories (eventCategoryId,eventCategoryName,eventDuration)
-value (5,'Server-side Clinic', 30);
-
-select * from eventCategories;
->>>>>>> 1d05caa3cf7575a8f75676af878e5df993cce47f
