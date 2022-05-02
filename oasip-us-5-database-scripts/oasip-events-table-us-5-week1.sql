c27ded17289553f5804295472abb1962d1eb06b3
Todsawat-Somtua <72536530+Todsawat-Somtua@users.noreply.github.com>
2022-05-02 16:11:23 +0700

    Update oasip-events-table-us-5-week1.sql
    database git conflict fixed.

--- oasip-us-5-database-scripts/oasip-events-table-us-5-week1.sql
+++ oasip-us-5-database-scripts/oasip-events-table-us-5-week1.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use oasipdatabase;
 insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
 value ('Somchai Jaidee(OR-7)', 'somchai.jai@mail.kmutt.ac.th', 2, '2022-5-23 13:30:00');
@@ -18,14 +17,5 @@ insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
 value ('Nut', 'Nutkung@kmutt.ac.th', '3', '2021-6-1 9:00:00');
 insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
 value ('OM', 'Ommi@kmutt.ac.th', '5', '2022-1-1 10:00:00');
-
-=======
-insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
-value ('Somchai Jaidee(OR-7)', 'somchai.jai@mail.kmutt.ac.th', 2, '2022-5-23 13:30:00');
-insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime, eventNotes)
-value ('SomsriRakdee(SJ-3)', 'somsri.rak@mail.kmutt.ac.th', 'Project Management Clinic', '27 April 2022 9.30', 'ขอปรึกษาปัญหาเพื่อนไม่ช`วยงาน');
-insert into events (bookingName, bookingEmail, eventCategoryId, eventStartTime)
-value ('สมเกียรติ ขยันเรียน กลุ`ม TT-4)', 'somkiat.kay@kmutt.ac.th', 'Database Clinic', '23 May 2022 16.30');
->>>>>>> 1d05caa3cf7575a8f75676af878e5df993cce47f
 
 select * from events;
