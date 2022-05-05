0f7f8453e474ceb20c6762215dd1247e93012d0c
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2022-05-06 05:25:44 +0700

    Update backend DTOs and Mapper

--- oasip-us-5-backend/src/main/java/sit/int221/oasipus5backend/services/EventService.java
+++ oasip-us-5-backend/src/main/java/sit/int221/oasipus5backend/services/EventService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 import sit.int221.oasipus5backend.entitires.Event;
 import sit.int221.oasipus5backend.repositories.EventRepository;
@@ -14,11 +15,15 @@ import java.util.List;
 public class EventService {
     @Autowired
     private EventRepository repository;
-    public List<Event> getEvent(){
-        return repository.findAll();
-    }
+    public List<Event> getEvent(){ return repository.findAll(); }
     public Event getEventDetail(@PathVariable Integer eventId) {
         return repository.findById(eventId).orElseThrow(
                 ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, eventId + " does not exit"));
     }
+    public Event createEvent(@RequestBody Event newEvent){ return repository.saveAndFlush(newEvent); }
+    public void deleteEvent(@PathVariable Integer eventId){ repository.findById(eventId).orElseThrow(
+            ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, eventId + " does not exist"));
+        repository.deleteById(eventId);
+    }
+
 }
