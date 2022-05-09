572a3d6cada4ef0a0447ef0e8f083311afe0056d
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2022-05-09 17:17:14 +0700

    Backend EventCategory

---
+++ oasip-us-5-backend/src/main/java/sit/int221/oasipus5backend/entitires/EventCategory.java
@@ -0,0 +1,32 @@
+package sit.int221.oasipus5backend.entitires;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "eventcategories")
+public class EventCategory {
+    @Id
+    @Column(name = "eventCategoryId", nullable = false)
+    private Integer eventCategoryId;
+
+    @Column(name = "eventCategoryName", nullable = false, length = 100)
+    private String eventCategoryName;
+
+    @Column(name = "eventDuration", nullable = false)
+    private Integer eventDuration;
+
+    @Column(name = "eventCategoryDescription", length = 300)
+    private String eventCategoryDescription;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "eventCategory")
+    private Set<Event> events = new LinkedHashSet<>();
+}
