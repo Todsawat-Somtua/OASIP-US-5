e9cb46c17ebea785bd5d23352fbca00e7d5afbb9
NattaponChusriwan <nattapon.chu@mail.kmutt.ac.th>
2022-05-09 17:10:44 +0700

    problem

--- oasip-us-5-backend/src/main/java/sit/int221/oasipus5backend/repositories/EventcategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package sit.int221.oasipus5backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int221.oasipus5backend.entitires.EventCategory;
-
-public interface EventCategoryRepository extends JpaRepository<EventCategory, Integer> {
-}
--- oasip-us-5-frontend/oasip-frontend/src/views/Home.vue
+++ oasip-us-5-frontend/oasip-frontend/src/views/Home.vue
@@ -16,40 +16,56 @@ const showModel = (e) => {
 const showAddEvent = (e) => {
   isAddEvent.value = e;
 };
-const isAddEvent = ref(false)
-const webUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : 'http://localhost:8080/api';
+const isAddEvent = ref(false);
+const webUrl = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL
+  : "http://localhost:8080/api";
 const currentEventDetail = ref({});
-
 
 const currentEvent = (event) => {
   currentEventDetail.value = event;
   isModel.value = true;
 };
 
-
 // Create
-const newestEvent = ref({})
+const newestEvent = ref({});
 const createNewEvent = async (newEvent) => {
-  if (newEvent.name === undefined || newEvent.genre === undefined) {
-    alert('please insert data')
-  } else {
-    const res = await fetch(`${webUrl}/events`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({ bookingName: newEvent.bookingName , bookingEmail : newEvent.bookingEmail  , eventStartTime: newEvent.eventStartTime, eventNotes:newEvent.eventNotes,eventCategory:newEvent.eventCategory})
-    })
-    if(res.status === 201) {
-    const addedEvent = await res.json()
-    eventsGetted.value.push(addedEvent)
-    alert('Added Successfully')
-    } else ('error, cannot added')
-    newestEvent.value = {}
-  }
+  console.log(newEvent.bookingName)
+    console.log(newEvent.bookingEmail)
+      console.log(newEvent.eventStartTime)
+        console.log(newEvent.eventCategoryId)
+                console.log(newEvent.eventNotes)
 }
+// const createNewEvent = async (newEvent) => {
+//   if (
+//     newEvent.bookingName === undefined ||
+//     newEvent.bookingEmail === undefined
+//   ) {
+//     alert("please insert data");
+//   } else {
+//     const res = await fetch(`${webUrl}/events`, {
+//       method: "POST",
+//       headers: {
+//         "content-type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         bookingName: newEvent.bookingName,
+//         bookingEmail: newEvent.bookingEmail,
+//         eventStartTime: newEvent.eventStartTime,
+//         eventNotes: newEvent.eventNotes,
+//         eventCategoryId: newEvent.eventCategoryId,
+//       }),
+//     });
+//     if (res.status === 201) {
+//       const addedEvent = await res.json();
+//       eventsGetted.value.push(addedEvent);
+//       alert("Added Successfully");
+//     } else "error, cannot added";
+//     newestEvent.value = {};
+//   }
+// };
 // Read
-const eventCategoriesGetter = ref([])
+const eventCategoriesGetter = ref([]);
 const getEvent = async () => {
   const res = await fetch(`${webUrl}/events`);
   if (res.status === 200) {
@@ -58,19 +74,19 @@ const getEvent = async () => {
   } else console.log("error, cannot get data");
 };
 const getEventCategory = async () => {
-  const res = await fetch(`${webUrl}/eventCategories`)
+  const res = await fetch(`${webUrl}/eventCategories`);
   if (res.status === 200) {
-    eventCategoriesGetter.value = await res.json()
-    console.log(eventCategoriesGetter.value)
-  } else console.log("error, cannot get data")
-}
+    eventCategoriesGetter.value = await res.json();
+    console.log(eventCategoriesGetter.value);
+  } else console.log("error, cannot get data");
+};
 
 onBeforeMount(async () => {
-  await getEvent()
-  await getEventCategory()
-  eventsGetted.value.sort((a, b) =>
-    moment(b.eventStartTime) - moment(a.eventStartTime)
-);
+  await getEvent();
+  await getEventCategory();
+  eventsGetted.value.sort(
+    (a, b) => moment(b.eventStartTime) - moment(a.eventStartTime)
+  );
 });
 
 // Update
@@ -78,30 +94,36 @@ onBeforeMount(async () => {
 const removeEvent = async (deleteEventId) => {
   if (confirm(`Do you want to delete event-id: ${deleteEventId} `) == true) {
     const res = await fetch(`${webUrl}/events/${deleteEventId}`, {
-    method: 'DELETE'
-    })
+      method: "DELETE",
+    });
     if (res.status === 200) {
-      eventsGetted.value = eventsGetted.value.filter((event) => event.id !== deleteEventId)
-      console.log('deleted successfully')
-    } else console.log('error, cannot delete data')
+      eventsGetted.value = eventsGetted.value.filter(
+        (event) => event.id !== deleteEventId
+      );
+      console.log("deleted successfully");
+    } else console.log("error, cannot delete data");
   } else {
-    console.log('cancel')
+    console.log("cancel");
   }
-}
-
+};
 </script>
 
 <template>
-<div class="grid justify-items-end" v-show="eventsGetted.length !== 0">
-  <AddEventIcon @click="isAddEvent =! isAddEvent"/>
-</div>
+  <div class="grid justify-items-end" v-show="eventsGetted.length !== 0">
+    <AddEventIcon @click="isAddEvent = !isAddEvent" />
+  </div>
   <div v-show="isAddEvent">
-    <add-event :events="eventsGetted" :eventCategories="eventCategoriesGetter" @close="showAddEvent"></add-event>
+    <add-event
+      :events="eventsGetted"
+      :eventCategories="eventCategoriesGetter"
+      @close="showAddEvent"
+      @addEvent="createNewEvent"
+    ></add-event>
   </div>
   <!-- event empty -->
   <div v-if="eventsGetted.length === 0">
-    <div class="flex justify-center mt-40 text-gray-400" >
-      <base-button buttonName="Add Event" @click="isAddEvent =! isAddEvent"/>
+    <div class="flex justify-center mt-40 text-gray-400">
+      <base-button buttonName="Add Event" @click="isAddEvent = !isAddEvent" />
     </div>
     <div class="flex justify-center text-7xl text-gray-400 mt-10">
       No schedule
@@ -109,7 +131,11 @@ const removeEvent = async (deleteEventId) => {
   </div>
   <!-- Show event -->
   <div v-else>
-    <events-list :events="eventsGetted" @passEvent="currentEvent" @deleteEvent="removeEvent" />
+    <events-list
+      :events="eventsGetted"
+      @passEvent="currentEvent"
+      @deleteEvent="removeEvent"
+    />
   </div>
   <div v-if="isModel">
     <show-detail :eventDetail="currentEventDetail" @close="showModel" />
